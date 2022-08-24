117f156160e7d9f50e6043ccceeb89e098c5e476
Lasse Lambrechts <lasse.lambrechts@bt.no>
2022-08-24 15:40:13 +0200

    fixed error introduced by code formatter
    Signed-off-by: Lasse Lambrechts <lasse.lambrechts@bt.no>

--- src/dataframe/clean_column_headers.py
+++ src/dataframe/clean_column_headers.py
@@ -31,13 +31,13 @@ def clean_column_header(column_header, whitelist, replace):
     cleaned_column_header = ''.join(c for c in cleaned_column_header if c in whitelist)
     return cleaned_column_header[:char_limit]
 
-def clean_column_headers(_df, whitelist=valid_filename_chars, replace='-'):
+def clean_column_headers(_df, whitelist=valid_filename_chars, replace=[" ", "-"]):
     """Makes column headers usable in pandas
 
     Args:
         _df (Dataframe): The Dataframe with the columns you wish to fix
         whitelist (str, optional): valid characters to use in the header, defaults to underscore, ascii letters and digits
-        replace (str, optional): What should you replace invalid characters with? Defaults to ' -'.
+        replace (array, optional): What should you replace invalid characters with? Defaults to [" ", "-"].
 
     Returns:
         Dataframe: A dataframe with cleaned column headers
