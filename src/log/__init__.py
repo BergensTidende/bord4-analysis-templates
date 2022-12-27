af71bba2c11263b639450d8b78c01ec8a5db3761
Lasse Lambrechts <lasse.lambrechts@bt.no>
2022-12-27 11:56:17 +0100

    colored logging and githook for removing output from notebooks
    Signed-off-by: Lasse Lambrechts <lasse.lambrechts@bt.no>

--- src/log/__init__.py
+++ src/log/__init__.py
@@ -1,8 +1,45 @@
-import logging
-import os
 import sys
+import os
+import logging
+from typing import Optional, Dict
 
-_log_fmt = '%(asctime)s - %(module)s - %(levelname)s - %(message)s'
-logging.basicConfig(level=os.environ.get('LOGLEVEL', 'INFO'), format=_log_fmt)
-_MODULE = sys.modules[__name__]
-logger = logging.getLogger(__name__)
+from .ansi import Back, Fore, Style
+
+class ColoredFormatter(logging.Formatter):
+    """Colored log formatter."""
+
+    def __init__(self, *args, colors: Optional[Dict[str, str]]=None, **kwargs) -> None:
+        """Initialize the formatter with specified format strings."""
+
+        super().__init__(*args, **kwargs)
+
+        self.colors = colors if colors else {}
+
+    def format(self, record) -> str:
+        """Format the specified record as text."""
+
+        record.color = self.colors.get(record.levelname, '')
+        record.reset = Style.RESET_ALL
+
+        return super().format(record)
+
+formatter = ColoredFormatter(
+    '{color}[{levelname:.1s}] {asctime}: {message}{reset}',
+    style='{', 
+    datefmt='%Y-%m-%d %H:%M:%S',
+    colors={
+        'DEBUG': Fore.LIGHTCYAN_EX,
+        'INFO': Fore.LIGHTGREEN_EX,
+        'WARNING': Fore.LIGHTYELLOW_EX,
+        'ERROR': Fore.LIGHTRED_EX,
+        'CRITICAL': Fore.RED + Back.WHITE + Style.BOLD,
+    }
+)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(formatter)
+
+logger = logging.getLogger()
+logger.handlers[:] = []
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
